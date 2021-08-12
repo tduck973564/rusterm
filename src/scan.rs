c4e351a9076dd29546bd912b28cb4b174bf99fa3
Thomas <tduck973564@gmail.com>
2021-08-12 18:11:52 +1000

    fix copyright message

--- src/scan.rs
+++ src/scan.rs
@@ -1,5 +1,5 @@
 // Copyright 2021 Thomas Duckworth <tduck973564@gmail.com>.
-// This file is part of the `brc` project, licenced under the GNU GPL v3.0, which can be read here: https://www.gnu.org/licenses/gpl-3.0.en.html
+// This file is part of the `rusterm` project, licenced under the GNU GPL v3.0, which can be read here: https://www.gnu.org/licenses/gpl-3.0.en.html
 
 //! Module for scanning and splitting up user input to be lexed.
 
